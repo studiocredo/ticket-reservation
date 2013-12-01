805a88495af19a684f59b6d34c76f2c62a52c86c
Thomas Matthijs <thomas.matthijs@selckin.be>
2013-12-01 17:48:26 +0100

    order links

--- app/controllers/Orders.scala
+++ app/controllers/Orders.scala
@@ -4,17 +4,18 @@ import com.google.inject.Inject
 import be.studiocredo.EventService
 import be.studiocredo.auth.{Authorization, Secure, AuthenticatorService}
 import play.api.mvc.Controller
-import models.ids.EventId
+import models.ids.{ShowId, EventId}
 
 class Orders @Inject()(eventService: EventService, val authService: AuthenticatorService) extends Controller with Secure {
 
   val defaultAuthorization = Some(Authorization.ANY)
 
 
-  def start(id: EventId) = AuthDBAction { implicit rs =>
-    eventService.get(id) match {
-      case None => BadRequest(s"Event $id not found")
-      case Some(event) => Ok(views.html.order(event))
-    }
+  def start(eventId: Option[Long], showId: Option[Long]) = AuthDBAction { implicit rs =>
+      Ok(s"hi $eventId $showId" )
+//    eventService.get(id) match {
+//      case None => BadRequest(s"Event $id not found")
+//      case Some(event) => Ok(views.html.order(event))
+//    }
   }
 }
