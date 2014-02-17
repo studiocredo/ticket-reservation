9cd247ba88e1e77053bb65f6209778beadaeb5b0
Sven Van Hemel <sven.vanhemel@gmail.com>
2014-02-17 16:39:15 +0100

    update

--- start_production.sh
+++ start_production.sh
@@ -7,5 +7,9 @@ fi
 dir=$(dirname "$0")
 prod_conf="$dir/scripts/private/production.conf"
 log_conf="$dir/scripts/private/production-logger.xml"
-opts="$cmd -Dconfig.file=$prod_conf -Dlogger.file=$log_conf -Dhttp.port=9000"
-sbt play "$opts"
+_JAVA_OPTIONS="-Dconfig.file=$prod_conf -Dlogger.file=$log_conf -Dhttp.port=9000"
+if [ "$cmd" == "start" ]; then
+  _JAVA_OPTIONS="$_JAVA_OPTIONS -Xms4G -Xmx4G"
+fi
+export _JAVA_OPTIONS
+sbt play "$cmd"
