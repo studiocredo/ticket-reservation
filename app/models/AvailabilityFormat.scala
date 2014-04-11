3cf1afbba752aee8974c14270cf49af2fb522221
Sven Van Hemel <sven.vanhemel@gmail.com>
2014-04-11 14:55:38 +0200

    show quantity

--- app/models/AvailabilityFormat.scala
+++ app/models/AvailabilityFormat.scala
@@ -5,7 +5,7 @@ import models.entities.{SeatType, ShowAvailability}
 object AvailabilityFormat {
   def format(availability: ShowAvailability): String = {
     availability.byType(SeatType.Normal) match {
-      case 0 => "Uitverkocht"
+      case quantity if quantity <= 0 => "Uitverkocht"
       case 1 => "Nog 1 plaats"
       case other => s"Nog $other plaatsen"
     }
