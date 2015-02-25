c0e1bfe66d658e2e4a5a5f9919fb3ffef9ae36e4
Sven Van Hemel <sven.vanhemel@gmail.com>
2015-02-25 21:52:54 +0100

    make showAll user detail configurable with parameter

--- app/controllers/admin/UserDetails.scala
+++ app/controllers/admin/UserDetails.scala
@@ -15,17 +15,17 @@ import models.entities._
 class UserDetails @Inject()(val userService: UserService, val authService: AuthenticatorService, val notificationService: NotificationService, orderService: OrderService, prereservationService: PreReservationService) extends AdminController with UserContextSupport {
   val logger = Logger("group-details")
 
-  def view(id: UserId) = AuthDBAction { implicit rs =>
+  def view(id: UserId, showAll: Boolean) = AuthDBAction { implicit rs =>
     userService.find(id) match {
       case None => BadRequest(s"Gebruiker $id niet gevonden")
-      case Some(details) => Ok(views.html.admin.user(getReservationHistory(details), userContext))
+      case Some(details) => Ok(views.html.admin.user(getReservationHistory(details, showAll), userContext))
     }
   }
 
-  def getReservationHistory(user: RichUser)(implicit s: DBSession): RichUserWithReservationHistory = {
+  def getReservationHistory(user: RichUser, showAll: Boolean)(implicit s: DBSession): RichUserWithReservationHistory = {
     val otherUsers = userService.findOtherUsers(user.user)
     val allUserIds = user.id :: otherUsers.map{_.id}
-    RichUserWithReservationHistory(user, otherUsers, orderService.detailedOrdersByUsers(allUserIds, Some(activeOrderFilter)), prereservationService.preReservationsByUsers(allUserIds, Some(activePreReservationFilter)), prereservationService.pendingPrereservationsByUsers(allUserIds, Some(activePreReservationFilter)), prereservationService.quotaByUsers(allUserIds, Some(activeQuotaFilter)), prereservationService.unusedQuotaByUsers(allUserIds, Some(activeQuotaFilter)))
+    RichUserWithReservationHistory(user, otherUsers, orderService.detailedOrdersByUsers(allUserIds, if (showAll) None else Some(activeOrderFilter)), prereservationService.preReservationsByUsers(allUserIds, if (showAll) None else Some(activePreReservationFilter)), prereservationService.pendingPrereservationsByUsers(allUserIds, if (showAll) None else Some(activePreReservationFilter)), prereservationService.quotaByUsers(allUserIds, if (showAll) None else Some(activeQuotaFilter)), prereservationService.unusedQuotaByUsers(allUserIds, if (showAll) None else Some(activeQuotaFilter)))
   }
 
   private def activeOrderFilter(t: (TicketOrder, TicketSeatOrder, Show, Event, Venue)): Boolean = {
