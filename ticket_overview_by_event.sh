e69d5172a7b91cbc57a0af064f5878c0eb327a2c
Sven Van Hemel <sven.vanhemel@gmail.com>
2015-12-01 22:46:40 +0100

    update

--- ticket_overview_by_event.sh
+++ ticket_overview_by_event.sh
@@ -1,5 +1,5 @@
 event=$1
 
 cat <<EOF
-Copy(select u.name, s.date, st.seat from "order" o join "user" u on o.user_id = u.id join "order-ticket" t on o.id = t.order_id join "order-ticket-seat" st on st.ticket_order_id = t.id join "show" s on t.show_id = s.id join "event" e on s.event_id = e.id where e.name = '$event' order by s.date, substring(st.seat from 1 for 1), 1 - to_number(substring(st.seat from 2),'99') % 2, to_number(substring(st.seat from 2),'99')) TO STDOUT WITH CSV;
+Copy(select u.name, s.date, st.seat, o.comments from "order" o join "user" u on o.user_id = u.id join "order-ticket" t on o.id = t.order_id join "order-ticket-seat" st on st.ticket_order_id = t.id join "show" s on t.show_id = s.id join "event" e on s.event_id = e.id where e.name = '$event' order by s.date, substring(st.seat from 1 for 1), 1 - to_number(substring(st.seat from 2),'99') % 2, to_number(substring(st.seat from 2),'99')) TO STDOUT WITH CSV;
 EOF
