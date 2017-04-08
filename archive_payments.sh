5c070db3954cec505b5cc4a2b288ac3a2df9f428
Sven Van Hemel <sven.vanhemel@gmail.com>
2017-04-08 10:39:59 +0200

    query to archive payments by date and amount

---
+++ archive_payments.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -e
+
+date=$1
+amount=$2
+
+die() {
+  local message="$1"
+  local code="${2:-1}"
+  if [[ -n "$message" ]] ; then
+    echo "ERROR: ${message}"
+  else
+    echo "ERROR: Execution failed"
+  fi
+  exit "${code}"
+}
+
+usage() {
+    echo "$(basename "$0") <date> [amount]"
+    echo "  date  : date, only archive payments before that date"
+    echo "  amount: optional amount to filter on"
+}
+
+if [ -z "$date" ]; then
+  usage
+  die 'Provide at least date'
+fi
+
+dateArg=$(date -d "$date" -I)
+
+if [ -n "$amount" ]; then
+cat <<EOF
+update "payment" set order_id = null, archived = true where amount = $amount and archived is false and date < date '$dateArg';
+EOF
+else
+cat <<EOF
+update "payment" set order_id = null, archived = true where date < date '$dateArg' and archived is false;
+EOF
+fi
+
+
