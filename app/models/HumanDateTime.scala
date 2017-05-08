3204d7af34d19b875162cafc3e5d9ce3c3ca7425
Sven Van Hemel <sven.vanhemel@gmail.com>
2017-05-08 20:46:49 +0200

    replace deprecated method

--- app/models/HumanDateTime.scala
+++ app/models/HumanDateTime.scala
@@ -5,16 +5,16 @@ import org.joda.time._
 import java.util.Locale
 
 object HumanDateTime {
-  val LOCALE = Locale.forLanguageTag("nl")
+  val LOCALE: Locale = Locale.forLanguageTag("nl")
 
-  val FORMATTER_DATE_TIME = DateTimeFormat.forPattern("EEEE d MMMM yyyy 'om' HH:mm").withLocale(LOCALE)
-  val FORMATTER_DATE_TIME_COMPACT = DateTimeFormat.forPattern("dd/MM/yyyy HH:mm").withLocale(LOCALE)
+  val FORMATTER_DATE_TIME: DateTimeFormatter = DateTimeFormat.forPattern("EEEE d MMMM yyyy 'om' HH:mm").withLocale(LOCALE)
+  val FORMATTER_DATE_TIME_COMPACT: DateTimeFormatter = DateTimeFormat.forPattern("dd/MM/yyyy HH:mm").withLocale(LOCALE)
 
-  val FORMATTER_DATE = DateTimeFormat.forPattern("EEEE d MMMM yyyy").withLocale(LOCALE)
-  val FORMATTER_DATE_COMPACT = DateTimeFormat.forPattern("dd/MM/yyyy").withLocale(LOCALE)
+  val FORMATTER_DATE: DateTimeFormatter = DateTimeFormat.forPattern("EEEE d MMMM yyyy").withLocale(LOCALE)
+  val FORMATTER_DATE_COMPACT: DateTimeFormatter = DateTimeFormat.forPattern("dd/MM/yyyy").withLocale(LOCALE)
 
   def formatDateOrDateTime(date: DateTime): String = {
-    if (date.isEqual(date.toDateMidnight)) {
+    if (date.isEqual(date.withTimeAtStartOfDay())) {
       formatDate(date)
     } else {
       formatDateTime(date)
@@ -29,11 +29,11 @@ object HumanDateTime {
 
   def formatDateCompact(date: DateTime): String = FORMATTER_DATE_COMPACT.print(date)
 
-  val ONE_DAY = Duration.standardDays(1)
-  val PERIOD_TYPE_HOUR_MINUTE = PeriodType.forFields(Array(DurationFieldType.hours(), DurationFieldType.minutes(), DurationFieldType.seconds()))
-  val PERIOD_TYPE_YEAR_MONTH_DAY_HOUR = PeriodType.forFields(Array(DurationFieldType.years(), DurationFieldType.months(), DurationFieldType.days(), DurationFieldType.hours()))
+  val ONE_DAY: Duration = Duration.standardDays(1)
+  val PERIOD_TYPE_HOUR_MINUTE: PeriodType = PeriodType.forFields(Array(DurationFieldType.hours(), DurationFieldType.minutes(), DurationFieldType.seconds()))
+  val PERIOD_TYPE_YEAR_MONTH_DAY_HOUR: PeriodType = PeriodType.forFields(Array(DurationFieldType.years(), DurationFieldType.months(), DurationFieldType.days(), DurationFieldType.hours()))
   val VARIANTS = Array(" ", ",", ", en ", ", en ")
-  val PERIOD_FORMATTER = new PeriodFormatterBuilder()
+  val PERIOD_FORMATTER: PeriodFormatter = new PeriodFormatterBuilder()
       .appendYears().appendSuffix(" jaar", " jaren")
       .appendSeparator(", ", " en ", VARIANTS).appendMonths().appendSuffix(" maand", " maanden")
       .appendSeparator(", ", " en ", VARIANTS).appendWeeks().appendSuffix(" week", " weken")
