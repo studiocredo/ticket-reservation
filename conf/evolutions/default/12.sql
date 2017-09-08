806c31a1b5786133c3f84c12b939dc165b412288
Sven Van Hemel <sven.vanhemel@gmail.com>
2017-09-08 17:08:19 +0200

    default values

--- conf/evolutions/default/12.sql
+++ conf/evolutions/default/12.sql
@@ -1,6 +1,7 @@
 # --- !Ups
 
 alter table "event" add column "quota" TEXT;
+update "event" set quota = '{"default":10,"values":[{"u":1,"v":5},{"u":2,"v":8},{"u":3,"v":10}]}'  where name like 'Slotshow %';
 
 
 # --- !Downs
