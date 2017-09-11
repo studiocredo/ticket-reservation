549b1afebab80f7878150bed85763602f1fca0dd
Sven Van Hemel <sven.vanhemel@gmail.com>
2017-09-11 11:17:35 +0200

    production script

--- start_production.sh
+++ start_production.sh
@@ -3,11 +3,15 @@ cmd=$1
 if [ -z "$cmd" ]; then
   cmd=start
 fi
+shift
 
 dir=$(dirname "$0")
-prod_conf="$dir/scripts/private/production.conf"
-log_conf="$dir/scripts/private/production-logger.xml"
-_JAVA_OPTIONS="-Dconfig.file=$prod_conf -Dlogger.file=$log_conf -Dhttp.port=9000 -DapplyEvolutions.default=true"
+conf_dir="$dir/scripts/private"
+pkFile=$(find "$conf_dir" -name '*.der' | head -1)
+conf="$conf_dir/application.production.conf"
+log_resource="production-logger.xml"
+
+_JAVA_OPTIONS="-Dconfig.file=$conf -Daws.key-pair.private-key.path=$pkFile -Dlogger.resource=$log_resource -Dhttp.port=9000 -DapplyEvolutions.default=true"
 if [ "$cmd" == "start" ]; then
   _JAVA_OPTIONS="$_JAVA_OPTIONS -Xms16G -Xmx16G"
 fi
